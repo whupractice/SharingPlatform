f00361fdc4c11a0683583231ff454546d6a361ef
supeuro <supeuro@outlook.com>
2019-06-30 22:10:09 +0800

    完成根据学校名查询课程功能

--- src/main/java/com/example/demo/repository/LessonRepository.java
+++ src/main/java/com/example/demo/repository/LessonRepository.java
@@ -25,7 +25,7 @@ public interface LessonRepository extends JpaRepository<LessonEntity,Long>, JpaS
     @Query(value = "select distinct credit,education,end_time,img_link,is_excellent,lesson_intro,lesson_name,school_name,share_num,start_time,status,subject,welcome from lesson order by subject",nativeQuery = true)
     List<Object> getAllDistinctly();
 
-    //查询含有关键词的课程并去重
+    //查询含有关键词的课程
     //@Query(value = "select distinct credit,education,end_time,img_link,is_excellent,lesson_intro,lesson_name,school_name,share_num,start_time,status,subject,welcome from lesson where lesson_name like ?1 order by subject",nativeQuery = true)
     @Query(value = "select * from lesson where lesson_name like ?1 order by subject",nativeQuery = true)
     List<Object> getLessonByKeyword(String keyword);
@@ -41,6 +41,10 @@ public interface LessonRepository extends JpaRepository<LessonEntity,Long>, JpaS
     //根据学科查询课程
     @Query(value = "select * from lesson where subject = ?1",nativeQuery = true)
     List<LessonEntity> getBySubject(String subject);
+
+    //根据学校名查询课程
+    @Query(value = "select * from lesson where school_name = ?1",nativeQuery = true)
+    List<LessonEntity> getBySchoolName(String schoolName);
 
     //根据状态和学科查询课程
     @Query(value = "select * from lesson where status = ?1 and subject = ?2",nativeQuery = true)
