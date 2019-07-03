266e2ad60df3d8913874de355acd0457836bfa36
wangruiqi <2521107174@qq.com>
2019-07-03 18:41:31 +0800

    教师界面的前后端连接

--- src/main/resources/static/page/js/teacher.js
+++ src/main/resources/static/page/js/teacher.js
@@ -12,6 +12,14 @@ app.controller('teacherCtrl', function ($scope, $http, $stateParams,$state) {//$
     $scope.t_lesson = null;//课程
 
 
+    $scope.selectTLesson = null;//选择的课程
+
+
+    $scope.currentPage = 1;//当前页数
+    $scope.totalPage = 1;//总页数
+    $scope.pages = [];
+
+
     // $http({
     //     method: 'GET',
     //     url: '/teacher/id',
@@ -25,17 +33,45 @@ app.controller('teacherCtrl', function ($scope, $http, $stateParams,$state) {//$
 
     $scope.initTeacher = function () {
         $scope.teacher = $stateParams.teacher;//获取传来的老师
+        // $http({
+        //     method: 'GET',
+        //     url: '/teacher/id',
+        //     params:{
+        //         "id" : $scope.teacher.id
+        //     }
+        // }).then(function successCallback(response) {
+        //     $scope.se = response.data;//获取返回的数据
+        // });
+        $scope.getTLessons();
+    };
+
+
+
+
+    /**
+     * @Author      : Theory
+     * @Description : 跳转到课程详情页
+     * @Param       : 被点击的课程
+     */
+    $scope.goDetail_course = function (lesson) {
+        $state.go('courseinfo', {
+            "lesson": lesson
+        });
+    };
+
+    //发送请求
+    $scope.getTLessons = function () {
+        let p = $scope.currentPage-1;
         $http({
             method: 'GET',
-            url: '/teacher/id',
+            url: "tl/getLessonByTeacherId",
             params:{
-                "id" : $scope.teacher.teacherId
+                "teacherId" : $scope.teacher.teacherId
             }
         }).then(function successCallback(response) {
-            $scope.teacher = response.data;//获取返回的数据
-        });
-    }
-
+            $scope.selectTLesson = response.data.content;//获取返回的课程
+        })
+    };
 
 
 });
