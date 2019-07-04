0d0a1d7f49792a04430d36a9d98c6d00e606a5ba
supeuro <supeuro@outlook.com>
2019-07-04 09:46:22 +0800

    更新分页显示数量

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -128,7 +128,7 @@ public class StudentController {
 
     @ApiOperation(value = "分页获取所有课程管理员", notes = "分页获取所有课程管理员",httpMethod = "GET")
     @GetMapping("/lessonManagerPages")
-    public Page<StudentEntity> getLessonManagerPages(@PageableDefault(size = 12, sort = {"academyName"}, direction = Sort.Direction.DESC)@ApiParam(value = "分页信息") Pageable pageable) {
+    public Page<StudentEntity> getLessonManagerPages(@PageableDefault(size = 5, sort = {"academyName"}, direction = Sort.Direction.DESC)@ApiParam(value = "分页信息") Pageable pageable) {
 
         Specification<StudentEntity> specification = createLessonManagerSpecification();
         return studentService.getAll(specification,pageable);
@@ -136,7 +136,7 @@ public class StudentController {
 
     @ApiOperation(value = "根据姓名分页获取所有课程管理员", notes = "根据姓名分页获取所有课程管理员",httpMethod = "GET")
     @GetMapping("/lessonManagerPagesByName")
-    public Page<StudentEntity> getLessonManagerPagesByName(@PageableDefault(size = 12, sort = {"academyName"}, direction = Sort.Direction.DESC)@ApiParam(value = "分页信息") Pageable pageable,
+    public Page<StudentEntity> getLessonManagerPagesByName(@PageableDefault(size = 5, sort = {"academyName"}, direction = Sort.Direction.DESC)@ApiParam(value = "分页信息") Pageable pageable,
                                                            @RequestParam(value = "realName")@ApiParam(value = "课程管理员名") String realName) {
 
         Specification<StudentEntity> specification = createLessonManagerSpecificationByName(realName);
@@ -145,7 +145,7 @@ public class StudentController {
 
     @ApiOperation(value = "根据学校分页获取所有课程管理员", notes = "根据学校分页获取所有课程管理员",httpMethod = "GET")
     @GetMapping("/lessonManagerPagesBySchool")
-    public Page<StudentEntity> getLessonManagerPagesBySchool(@PageableDefault(size = 12, sort = {"academyName"}, direction = Sort.Direction.DESC)@ApiParam(value = "分页信息") Pageable pageable,
+    public Page<StudentEntity> getLessonManagerPagesBySchool(@PageableDefault(size = 5, sort = {"academyName"}, direction = Sort.Direction.DESC)@ApiParam(value = "分页信息") Pageable pageable,
                                                              @RequestParam(value = "schoolName")@ApiParam(value = "学校名") String schoolName) {
 
         Specification<StudentEntity> specification = createLessonManagerSpecificationBySchool(schoolName);
