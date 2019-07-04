a46981859e2c3eaf1e5206d9a5bde5e24d234bb1
PML17 <43109575+PML17@users.noreply.github.com>
2019-07-04 11:41:02 +0800

    评论区功能实现

--- src/main/resources/static/page/js/courseinfo.js
+++ src/main/resources/static/page/js/courseinfo.js
@@ -10,15 +10,18 @@ API_index.controller("courseinfoCtrl", function ($scope, $http, $state,$statePar
 
     $scope.lesson = null;
 
-
+$scope.sl=null;//选课信息
     $scope.teacherNum = 0;
 
     $scope.scores=[];//课程打分
 
     $scope.now = 1;//当前位置
 
-
+$scope.Si=null;//Studentinformation
     $scope.teachers = null;
+
+    $scope.currentScore = 4.3;
+    $scope.f = 0.3;
 
 
 
@@ -30,6 +33,8 @@ API_index.controller("courseinfoCtrl", function ($scope, $http, $state,$statePar
     $scope.initCourseInfo = function(){
         $scope.lesson = $stateParams.lesson;//接收传来课程信息
        $scope.getTeachers();
+       //$scope.getScores();
+       $scope.getComments();
     };
 
 
@@ -87,12 +92,70 @@ API_index.controller("courseinfoCtrl", function ($scope, $http, $state,$statePar
         });
     };
 
-
+//跳转到教师页面
     $scope.jumpT = function (teacher) {
         $state.go('teacher',{
             "teacher" : teacher
         });
+    };
+
+
+    //课程打分
+    $scope.getScores=function () {
+        var score=Math.floor(4.3);
+        $scope.f = 4.3 - score;
+        for(let i=0;i<score;i++)
+        {
+            $scope.scores.push(1);
+        }
+            };
+    $scope.judge=function () {
+        return $scope.f>0.5;
+    };
+    $scope.judge2=function () {
+        return $scope.f<=0.5;
+    };
+
+//获取评论信息
+    $scope.getComments=function () {
+        let lessonId = $scope.lesson.lessonId;//获取课程id
+        $http({
+            method:'GET',
+            url:'/sl/lessonId',
+            params:{
+                "lessonId" : lessonId
+            }
+        }).then(function successCallback(response) {
+            $scope.sl=response.data;
+            for(let i = 0;i<$scope.sl.length;i++){
+                let sl = $scope.sl[i];
+                $http({
+                    method: 'GET',
+                    url: '/student/info',
+                    params: {
+                        "phone": sl.phone
+                    }
+                }).then(function successCallback(response) {
+                    ($scope.sl[i])["nickName"] = response.data.nickName;
+                })
+            }
+        })
     }
+
+// //根据获取昵称
+//     $scope.getNickname=function () {
+//         let lessonId = $scope.lesson.lessonId;//获取课程id
+//         $http({
+//             method:'GET',
+//             url:'/sl/getStudentByLessonId',
+//             params:{
+//                 "lessonId":lessonId
+//             }
+//         }).then(function successCallback(response) {
+//             $scope.Nn=response.data;
+//         })
+//
+//     }
 
 });
 
