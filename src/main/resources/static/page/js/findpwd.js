c15eaf9b48f498e770dceab1ef61665510c86214
wangruiqi <2521107174@qq.com>
2019-07-05 10:39:02 +0800

    找回密码功能完成

--- src/main/resources/static/page/js/findpwd.js
+++ src/main/resources/static/page/js/findpwd.js
@@ -13,8 +13,7 @@ app.controller('findpwdCtrl', function ($scope, $http, $state,Data) {
     $scope.nowPage = 1;
 
 
-
-    //
+    //验证手机号
     $scope.verify_resetPwd = function(page){
 
         let user = $('#p_number').val();
@@ -50,8 +49,48 @@ app.controller('findpwdCtrl', function ($scope, $http, $state,Data) {
     $scope.code_page_2 = function () {
         return $scope.nowPage == 2;
     };
-    //重置密码
 
+
+    //重置密码
+    $scope.reset_pwd = function(){
+
+        let pwd = $('#new_pwd_1').val();
+        let phoneNumber = $scope.currentUser.phone;
+        let nickName = $scope.currentUser.nickName;
+        let sex = $scope.currentUser.sex;
+        let email = $scope.currentUser.email;
+        let birth = $scope.currentUser.birth;
+        let introduction = $scope.currentUser.introduction;
+
+        if ( pwd == $('#new_pwd_2').val()){
+            $http({
+                method: 'PUT',
+                url: '/student',
+                //如果swagger文档里参数是body类型，参数用data传递；若为query，参数用params
+                data: {
+                    "birth": birth,
+                    "email": email,
+                    "introduction": introduction,
+                    "nickName": nickName,
+                    "phone": phoneNumber,
+                    "pwd": pwd,
+                    "sex": sex
+                }
+            }).then(function successCallback(response) {
+                if (response.data == true) {
+                    alert('修改密码成功')
+
+                } else {
+                    alert("修改密码失败")
+                }
+                $state.go('login');
+
+            })
+        }
+        else {
+            alert("两次密码输入不一致！")
+        }
+    };
 
 
 })
