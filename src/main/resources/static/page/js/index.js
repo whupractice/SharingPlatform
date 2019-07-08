ca47a1259f1eadc730cd33554dabb26706a8444c
theoryxsy <2017302580167@whu.edu.com>
2019-07-08 16:27:03 +0800

    路由地址消除#！

--- src/main/resources/static/page/js/index.js
+++ src/main/resources/static/page/js/index.js
@@ -23,9 +23,9 @@ API_index.factory('Data',function () {
 });
 
 
-API_index.config(function ($stateProvider,$urlRouterProvider) {
+API_index.config(function ($stateProvider,$urlRouterProvider,$locationProvider) {
 
-
+    $locationProvider.html5Mode(true);
     /**
       * @Author      : Theory
       * @Description : 设置路由
