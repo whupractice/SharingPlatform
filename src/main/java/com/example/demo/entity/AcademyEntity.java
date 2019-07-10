4a3fdda6bf6217550fdd1d93989f9fb3890f1aa8
Yingran Qin <supeuro@outlook.com>
2019-07-10 09:24:04 +0800

    debug

--- src/main/java/com/example/demo/entity/AcademyEntity.java
+++ src/main/java/com/example/demo/entity/AcademyEntity.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "academy")
 public class AcademyEntity {
-    AcademyEntity(){}
+    public AcademyEntity(){}
 
     @Id
     @GeneratedValue
